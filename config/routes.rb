c84b75ef7e506aca7cf88dd2478d144535cd2b4c
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-19 12:31:22 +0100

    git add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  # get 'users/index'
+  # resources :posts
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
