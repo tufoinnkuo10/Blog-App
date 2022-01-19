db05ea533d82b284fb8c20aeae8d760edff61115
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-19 13:06:27 +0100

    add rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # get 'users/index'
   # resources :posts
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
 
-  root to: 'users#index'
+  # root to: 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
