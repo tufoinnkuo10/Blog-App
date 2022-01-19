6040783b484d208e5b42b301df08793aa2b452b1
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-19 12:43:47 +0100

    add user html

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
+
+  root to: 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
