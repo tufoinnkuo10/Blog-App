b2518e2023e0e7f14036c5c7086b97aee9d10028
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-20 07:40:47 +0100

    Add test for user index

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,2 +1,18 @@
 require 'rails_helper'
 
+RSpec.describe 'USERS', type: :request do
+  describe 'USERS GET #index' do
+    before(:example) { get('/users#index') }
+
+    it 'server returns 200 then server is ok' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'template is rendering correctly' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'shows correct body content of index' do
+      expect(response.body).to include('<h1>list of users</h1>')
+    end
+  end
