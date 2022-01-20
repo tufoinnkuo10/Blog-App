11b7f977052c3728b672d3d5f80d685d1fd7a0d0
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-20 15:36:51 +0100

    Add test and rubocop

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'POSTS', type: :request do
-  describe 'GET #index' do
+  describe 'GET POSTS #index' do
     before(:each) { get user_posts_path user_id: 20 }
 
     it 'server return 200 then server is ok' do
@@ -13,23 +13,23 @@ RSpec.describe 'POSTS', type: :request do
     end
 
     it 'shows body content of index' do
-      expect(response.body).to include('<h1>users posts</h1>')
+      expect(response.body).to include('<h1>show user posts details id</h1>')
     end
   end
 
-  describe 'GET  #show' do
+  describe 'GET POSTS #show' do
     before(:each) { get user_posts_path user_id: 20, id: 30 }
 
     it 'return 200' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'shows the show template' do
+    it 'shows the correct show template' do
       expect(response).to render_template(:show)
     end
 
-    it 'shows body content of posts/show' do
-      expect(response.body).to include("<h1>show user posts details</h1>")
+    it 'shows body content of posts show' do
+      expect(response.body).to include('<h1>show user posts details</h1>')
     end
   end
 end
