f587b859861892b88ce874c3eaf38d01bb2ac206
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-24 19:31:21 +0100

    add model class for user, add associations and method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :likes, through: :posts
+  has_many :comments, through: :posts
+  def three_recent_user
+    User.limit(3).order(created_at: :asc)
+  end
 end
