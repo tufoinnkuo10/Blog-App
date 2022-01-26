d02b2d2fbb9e5a7154ba24e062d27568c1d02b4c
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-26 09:14:06 +0100

    Add users controler code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.first(5)
+  end
 
-  def show; end
+  def show
+    @user = User.includes(:posts).find(params[:id])
+  end
 end
