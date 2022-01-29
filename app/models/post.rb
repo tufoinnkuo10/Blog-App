43c7b47e5c35faa4a4c964df1e117bdc6466c9cf
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-29 11:15:53 +0100

    vaIidate user P0STS

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,15 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   has_many :comments, foreign_key: 'post_id'
 
+  validates :title, length: { maximum: 250, minimum: 2 }, presence: true
+  validates :text, presence: true
+  validates :commentscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likescounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :posts_counter
+
   def post_counter_for_user(user)
     user.increment!(:posts_counter)
-  end
-
-  def like(user)
-    likes << Like.new(user_id: user)
   end
 
   def five_recent_comments()
